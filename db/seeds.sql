2cb19f5b9fa8c39c7feadb1b3f579bc45ba53b38
herbiemolina <herbiemolina@gmail.com>
2019-04-23 15:40:03 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("blueberry", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("buffalo", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("portabella", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("grass_feed", true);
