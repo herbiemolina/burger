ca0ba5d5d3ac3374266abb219644a681622142a5
herbiemolina <herbiemolina@gmail.com>
2019-04-23 15:32:20 -0400

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,13 @@
-CREATE DATABASE 
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers ( 
+  id Int AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR( 255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  
+
+  PRIMARY KEY ( `id` ) 
+);
