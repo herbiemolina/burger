f71bf4c3a6725d93ee392beade3515712eb95be1
herbiemolina <herbiemolina@gmail.com>
2019-04-23 21:38:05 -0400

    SOME FUNCTIONALITY

---
+++ eat-a-burger/public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+$(function() {
+  $(".change-burger").on("click", function(event) {
+    var id = $(this).data("id");
+    var newburger = $(this).data("newburger");
+
+    var newburgerState = {
+      devour: newburger
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: newburgerState
+    }).then(
+      function() {
+        console.log("changed burger to", newburger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newburger = {
+      burger_name: $("#ca").val().trim(),
+      
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newburger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+ 
+});
